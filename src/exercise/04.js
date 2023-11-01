eba1ebc00d92b449a2c02a370524c3d01540e486
Eduardo Pereira <egpereira05@gmail.com>
2023-11-01 19:13:43 -0300

    Add exercise 4 solution

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -3,10 +3,9 @@
 
 import * as React from 'react'
 
-function MessagesDisplay({messages}) {
+function MessagesDisplay({ messages }) {
   const containerRef = React.useRef()
-  // 🐨 replace useEffect with useLayoutEffect
-  React.useEffect(() => {
+  React.useLayoutEffect(() => {
     containerRef.current.scrollTop = containerRef.current.scrollHeight
   })
 
@@ -22,17 +21,13 @@ function MessagesDisplay({messages}) {
   )
 }
 
-// this is to simulate major computation/big rendering tree/etc.
 function sleep(time = 0) {
   const wakeUpTime = Date.now() + time
-  while (Date.now() < wakeUpTime) {}
+  while (Date.now() < wakeUpTime) { }
 }
 
 function SlooooowSibling() {
-  // try this with useLayoutEffect as well to see
-  // how it impacts interactivity of the page before updates.
   React.useEffect(() => {
-    // increase this number to see a more stark difference
     sleep(300)
   })
   return null
@@ -51,7 +46,7 @@ function App() {
 
   return (
     <div className="messaging-app">
-      <div style={{display: 'flex', justifyContent: 'space-between'}}>
+      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <button onClick={addMessage}>add message</button>
         <button onClick={removeMessage}>remove message</button>
       </div>
@@ -96,4 +91,4 @@ const allMessages = [
   `Leia: Don't just stand there. Try to brace it with something.`,
   `Luke: Wait a minute!`,
   `Luke: Threepio! Come in Threepio! Threepio! Where could he be?`,
-].map((m, i) => ({id: i, author: m.split(': ')[0], content: m.split(': ')[1]}))
+].map((m, i) => ({ id: i, author: m.split(': ')[0], content: m.split(': ')[1] }))
